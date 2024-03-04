9fca6d733d52bf067ec18cdd9f92f14d92d4bca3
Sentient Bit <60573913+sentientbit@users.noreply.github.com>
2024-03-04 11:18:22 +0200

    Create esat-class-customization.php

---
+++ core/classes/esat-class-customization.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ESAT\Classes;
+
+// =============================================================================
+// Class related to Customization features
+// @since 1.0.0
+// =============================================================================
+
+class Customization
+{
+    /**
+    * Function description
+    * 
+    * @since 1.0.0
+    */
+}
