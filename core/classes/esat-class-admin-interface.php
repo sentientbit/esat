62c893523b0f4a63eabca22832bc2fa01af34523
Sentient Bit <60573913+sentientbit@users.noreply.github.com>
2024-03-04 11:17:39 +0200

    Create esat-class-admin-interface.php

---
+++ core/classes/esat-class-admin-interface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ESAT\Classes;
+
+// =============================================================================
+// Class related to Admin Interface features
+// @since 1.0.0
+// =============================================================================
+
+class Admin_Interface
+{
+    /**
+    * Function description
+    * 
+    * @since 1.0.0
+    */
+}
