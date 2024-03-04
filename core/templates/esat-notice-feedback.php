d9211bcf8b0d754491f99d69cc0c613480d43f95
Sentient Bit <60573913+sentientbit@users.noreply.github.com>
2024-03-04 11:21:34 +0200

    Create esat-notice-feedback.php

---
+++ core/templates/esat-notice-feedback.php
@@ -0,0 +1,24 @@
+<?php
+$neveragain;
+
+?>
+
+<style>
+#wpnotice-esat-review a {
+    text-decoration: none!important;
+}
+</style>
+
+<div style="display: flex; flex-wrap: nowrap; gap: 15px; align-items: center;" id="wpnotice-esat-review" class="wpnotice-wrapper notice essential-settings-and-tools  notice-info notice-essential-settings-and-tools-review is-dismissible">
+    <div class="wpnotice-thumbnail-wrapper"><img style="max-width: 80px" src="<?php echo ESAT_URL . 'assets/admin/images/esat-icon-color.webp'; ?>"></div>
+    <div class="wpnotice-content-wrapper">
+        <p>We hope you're enjoying ESAT - Essential Settings and Tools! Could you please do us a BIG favor and give it a 5-star rating on WordPress to help us spread the word and boost our motivation?</p>
+        <ul style="display: flex; width: 100%; align-items: center;" class="notice-links essential-settings-and-tools-notice-links">
+            <li style="margin: 0 15px 0 0;" class="notice-link-item "><a href="https://wpdeveloper.com/review-essential-addons-elementor"><span style="margin-right: 5px" class="dashicons dashicons-external"></span>Ok, you deserve it!</a></li>
+            <li style="margin: 0 15px 0 0;" id="dismiss-btn-already-did" class="notice-link-item "><a href="#" class="dismiss-btn " target="_blank" data-dismiss="1"><span style="margin-right: 5px" class="dashicons dashicons-smiley"></span>I already did</a></li>
+            <li style="margin: 0 15px 0 0;" class="notice-link-item "><a href="#" class="dismiss-btn " target="_blank" data-later="1"><span style="margin-right: 5px" class="dashicons dashicons-calendar-alt"></span>Maybe Later</a></li>
+            <li style="margin: 0 15px 0 0;" class="notice-link-item "><a href="https://sentientbit.com/esat#faq" class="dismiss-btn " target="_blank" data-later="1"><span style="margin-right: 5px" class="dashicons dashicons-sos"></span>I need help</a></li>
+            <li style="margin: 0 15px 0 0;" class="notice-link-item "><a href="#" class="dismiss-btn " target="_blank" data-dismiss="1"><span style="margin-right: 5px" class="dashicons dashicons-dismiss"></span>Never show again</a></li>
+        </ul>
+    </div><button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button>
+</div>
