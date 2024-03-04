ec8d879be1c5b8224ad16ec351086726b5689c4b
Sentient Bit <60573913+sentientbit@users.noreply.github.com>
2024-03-04 11:18:56 +0200

    Create class-login-logout.php

---
+++ core/classes/class-login-logout.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ESAT\Classes;
+
+// =============================================================================
+// Class related to Login & Logout features
+// @since 1.0.0
+// =============================================================================
+
+class Login_Logut
+{
+    /**
+    * Function description
+    * 
+    * @since 1.0.0
+    */
+}
