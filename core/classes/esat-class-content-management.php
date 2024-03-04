3503893bcb4f5ec2b95ebb7b7094b05660fd1b8d
Sentient Bit <60573913+sentientbit@users.noreply.github.com>
2024-03-04 11:18:05 +0200

    Create esat-class-content-management.php

---
+++ core/classes/esat-class-content-management.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ESAT\Classes;
+
+// =============================================================================
+// Class related to Content Management features
+// @since 1.0.0
+// =============================================================================
+
+class Content_Management
+{
+    /**
+    * Function description
+    * 
+    * @since 1.0.0
+    */
+}
