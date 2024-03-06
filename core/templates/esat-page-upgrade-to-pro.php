c3b2d31083b5b132c318d3805d9b640b94556f7d
Răzvan-Viorel Dumitrașc <33253358+razvan-viorel-dumitrasc@users.noreply.github.com>
2024-03-06 15:19:06 +0200

    Update esat-page-upgrade-to-pro.php

--- core/templates/esat-page-upgrade-to-pro.php
+++ core/templates/esat-page-upgrade-to-pro.php
@@ -1,19 +1,16 @@
-<?php
-
-echo '
-<div class="esap-wpcontent">
+<div class="esat-content-container">
     <div class="esap-container-get-pro">
         <div class="container col-xxl-8 px-4 py-5">
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
               <div class="col-10 col-sm-8 col-lg-6">
-                <img class="d-block mx-lg-auto img-fluid" src="' . ESAT_URL . 'assets/admin/images/esat-build-faster-and-more-secure.webp' . '" alt="" width="700" height="500" loading="lazy">
+                <img class="d-block mx-lg-auto img-fluid" src="<?php echo ESAT_URL; ?>assets/admin/images/esat-build-faster-and-more-secure.webp" alt="" width="700" height="500" loading="lazy">
               </div>
               <div class="col-lg-6">
                 <h2 class="display-5 fw-bold lh-1 mb-3">Build faster and more secure with ESAT</h2>
                 <p class="lead">Quickly design, customize and secure WordPress sites, the world’s most popular CMS open source toolkit. Make your life easier and let us do the hard work. We have you covered. Just install, make the settings, and enjoy your free time.</p>
                
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="esat-btn-get-pro btn-lg fw-bold" onclick="window.location.href = \'#pricing\'">
+                    <button type="button" class="esat-btn-get-pro btn-lg fw-bold" onclick="window.location.href = '#pricing'">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stars" viewBox="0 0 16 16">
                             <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/>
                         </svg>
@@ -24,7 +21,7 @@ echo '
             </div>
         </div>
         <!----------------------------------------------------------------------------->
-        <div class="container my-5">
+        <div class="container my-5 bg-white">
             <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
               <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
                 <h1 class="display-4 fw-bold lh-1">Unlock Features With ESAT Pro</h1>
@@ -34,7 +31,7 @@ echo '
                 </div>
               </div>
               <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
-                  <img class="rounded-lg-3" src="' . ESAT_URL . 'assets/admin/images/esat-unlock-features-with-pro.webp' . '" alt="" width="720">
+                  <img class="d-block mx-lg-auto img-fluid" src="<?php echo ESAT_URL; ?>assets/admin/images/esat-unlock-features-with-pro.webp" alt="" width="720" loading="lazy">
               </div>
             </div>
         </div>
@@ -46,7 +43,7 @@ echo '
         <div class="row" id="pricing">
             <div class="col">
                 <div class="card mb-4 rounded-3 shadow-sm">
-                    <div class="card-header py-3">
+                    <div class="card-header bg-white py-3">
                         <h4 class="my-0 fw-normal">Free</h4>
                     </div>
                     <div class="card-body">
@@ -66,7 +63,7 @@ echo '
             </div>
             <div class="col">
                 <div class="card mb-4 rounded-3 shadow-sm">
-                    <div class="card-header py-3">
+                    <div class="card-header bg-white py-3">
                         <h4 class="my-0 fw-normal">Lite</h4>
                     </div>
                     <div class="card-body">
@@ -86,7 +83,7 @@ echo '
             </div>
             <div class="col">
                 <div class="card mb-4 rounded-3 shadow-sm">
-                    <div class="card-header py-3">
+                    <div class="card-header bg-white py-3">
                         <h4 class="my-0 fw-normal">Pro</h4>
                     </div>
                     <div class="card-body">
@@ -127,7 +124,6 @@ echo '
         </div>
         <div class="esat-plans"  style="max-width:800px; margin: auto;">
             <div class="esat-plans-header p-3 pb-md-4 mx-auto text-center">
-                <h1 class="display-4 fw-normal">Pricing</h1>
                 <p class="fs-5 text-muted">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
             </div>
             <div class="table-responsive">
@@ -200,4 +196,3 @@ echo '
         </div>
     </div>
 </div>
-';
