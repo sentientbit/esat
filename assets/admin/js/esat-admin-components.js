79d0e5ce80dd9b260e1c82749e4fbd29e482412e
Răzvan-Viorel Dumitrașc <33253358+razvan-viorel-dumitrasc@users.noreply.github.com>
2024-03-06 15:11:29 +0200

    Update esat-admin-components.js

--- assets/admin/js/esat-admin-components.js
+++ assets/admin/js/esat-admin-components.js
@@ -1,3 +1,8 @@
+/**
+ * Security level progress
+ * 
+ *  @since 1.0.0
+ */
 $(document).ready(function(){
     $('.progress-value > span').each(function(){
         $(this).prop('Counter',0).animate({
@@ -10,4 +15,41 @@ $(document).ready(function(){
             }
         });
     });
+});
+
+/**
+ * Lock dropdown in settings
+ * 
+ *  @since 1.0.0
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    setupAccordionCheckbox("esat_security_option_1", "accordionSecurity_1");
+    setupAccordionCheckbox("esat_security_option_2", "accordionSecurity_2");
+
+    function setupAccordionCheckbox(checkboxId, accordionId) {
+        var checkbox = document.getElementById(checkboxId);
+        var accordion = document.getElementById(accordionId);
+
+        var isChecked = localStorage.getItem(checkboxId);
+        if (isChecked === "true") {
+            checkbox.checked = true;
+            accordion.classList.remove("disable");
+        } else {
+            checkbox.checked = false;
+            accordion.classList.add("disable");
+            accordion.querySelector(".accordion-collapse").classList.remove("show");
+        }
+
+        checkbox.addEventListener("change", function() {
+            if (this.checked) {
+                accordion.classList.remove("disable");
+                accordion.querySelector(".accordion-collapse").classList.add("show");
+                localStorage.setItem(checkboxId, "true");
+            } else {
+                accordion.classList.add("disable");
+                accordion.querySelector(".accordion-collapse").classList.remove("show");
+                localStorage.setItem(checkboxId, "false");
+            }
+        });
+    }
 });
