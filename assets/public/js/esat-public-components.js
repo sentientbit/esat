81675d6a5c3b58e29767605b278929ae0d49712d
Răzvan-Viorel Dumitrașc <33253358+razvan-viorel-dumitrasc@users.noreply.github.com>
2024-03-06 15:12:34 +0200

    Update esat-public-components.js

--- assets/public/js/esat-public-components.js
+++ assets/public/js/esat-public-components.js
@@ -1,3 +1,39 @@
+// =============================================================================
+// JS related to ESAT_Customization features
+// @since 1.0.3
+// =============================================================================
+
 /**
- * @since 1.0.0
- */
+* Preloader
+* 
+* @since 1.0.3
+*/
+document.addEventListener('DOMContentLoaded', function() {
+    // Ascunde preloader-ul după ce pagina este complet încărcată
+    var preloader = document.querySelector('.preloader');
+    if (preloader) {
+        preloader.style.display = 'none';
+    }
+});
+
+/**
+* Scroll to top
+* 
+* @since 1.0.4
+*/
+jQuery(document).ready(function($) {
+    $("#esat-scroll-to-top").hide();
+    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $("#esat-scroll-to-top").fadeIn();
+        } else {
+            $("#esat-scroll-to-top").fadeOut();
+        }
+    });
+    
+    $("#esat-scroll-to-top").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+});
